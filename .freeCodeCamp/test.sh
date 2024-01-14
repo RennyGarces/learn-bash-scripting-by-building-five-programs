67cff6db399aef1d5e3bdc1626229dc743dbe40b
renny <rennygarces1983@gmail.com>
2024-01-14 05:35:52 -0500

    Save progress: 1412.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -4,19 +4,29 @@ echo -e "\n~~ Fortune Teller ~~\n"
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
 N=$(( RANDOM % 6 ))
 
-GET_FORTUNE(){
+function GET_FORTUNE() {
+  if [[ ! $1 ]]
+  then
+    echo Ask a yes or no question:
+  
+else
+ echo Try again. Make sure it ends with a question mark:
 
-echo Ask a yes or no question:
 
-read QUESTION
+fi
 
+  read QUESTION
 }
 
-GET_FORTUNE
 
+
+until [[ $QUESTION =~ \?$ ]]
+do
+GET_FORTUNE
+done
 
 
 
 echo ${RESPONSES[$N]}
 
-echo $QUESTION
+GET_FORTUNE again
